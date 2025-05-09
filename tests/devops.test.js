9a6eaa68ece0468103f1e79fa683935c5779ac04
RicardoRangles <ricardo_rangles@hotmail.com>
2025-05-09 02:02:28 -0500

    Mejoras Pruebas Unitarias

--- tests/devops.test.js
+++ tests/devops.test.js
@@ -29,4 +29,45 @@ describe('POST /DevOps', () => {
 
     expect(response.status).toBe(403);
   });
+
+  it('should return 405 for GET requests', async () => {
+    const response = await request(app)
+      .get('/DevOps')
+      .set('X-Parse-REST-API-Key', '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c')
+      .send();
+
+    expect(response.status).toBe(405); // Method Not Allowed
+  });
+
+  it('should return 405 for PUT requests', async () => {
+    const response = await request(app)
+      .put('/DevOps')
+      .set('X-Parse-REST-API-Key', '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c')
+      .send();
+
+    expect(response.status).toBe(405); // Method Not Allowed
+  });
+
+  it('should return 405 for DELETE requests', async () => {
+    const response = await request(app)
+      .delete('/DevOps')
+      .set('X-Parse-REST-API-Key', '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c')
+      .send();
+
+    expect(response.status).toBe(405); // Method Not Allowed
+  });
+
+  it('should return 404 for invalid routes', async () => {
+    const response = await request(app)
+      .post('/InvalidRoute')
+      .set('X-Parse-REST-API-Key', '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c')
+      .send({
+        message: 'This is a test',
+        to: 'Juan Perez',
+        from: 'Rita Asturia',
+        timeToLifeSec: 45
+      });
+
+    expect(response.status).toBe(404); // Not Found
+  });
 });
